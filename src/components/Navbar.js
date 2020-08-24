73e57269c3d2894ca0ae74af67e5093a962cfdaa
Appy Sharma <appy70890@gmail.com>
2020-08-24 17:45:24 +0200

    added snackbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,22 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Navbar = ({ changeLevel, level, handleChange }) => {
   let [colorFormat, setColorFormat] = useState("hex");
+  let [openSnackbar, setOpenSnackbar] = useState(false);
 
-  const handleOnChange = (e) => {
+  const handleFormatChange = (e) => {
     setColorFormat(e.target.value);
     handleChange(e.target.value);
+    setOpenSnackbar(true);
+  };
+
+  const closeSnackbar = () => {
+    setOpenSnackbar(false);
   };
 
   return (
@@ -27,12 +36,32 @@ const Navbar = ({ changeLevel, level, handleChange }) => {
         />
       </div>
       <div className="select-container">
-        <Select value={colorFormat} onChange={handleOnChange}>
+        <Select value={colorFormat} onChange={handleFormatChange}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(155,155,255)</MenuItem>
           <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={closeSnackbar}
+        message={
+          <span id="message-id">{`Format changed to ${colorFormat.toUpperCase()}`}</span>
+        }
+        ContentProps={{ "aria-describedby": "message-id" }}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </nav>
   );
 };
